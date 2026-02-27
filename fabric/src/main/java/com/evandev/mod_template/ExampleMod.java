f17f01b1ad486b48ad45add0c6be976581fc28b1
evanbones <74276616+evanbones@users.noreply.github.com>
2026-02-26 20:11:48 -0800

    Refactor publishing and datagen

--- fabric/src/main/java/com/evandev/mod_template/ExampleMod.java
+++ fabric/src/main/java/com/evandev/mod_template/ExampleMod.java
@@ -1,56 +1,12 @@
 package com.evandev.mod_template;
 
-import com.evandev.mod_template.config.ModConfig;
-import com.evandev.mod_template.logic.RuleManager;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.ResourceReloadListenerKeys;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.PackType;
-import net.minecraft.server.packs.resources.ResourceManager;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.Collections;
 
 public class ExampleMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
         CommonClass.init();
-
-        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            CommonClass.setServer(server);
-            ModConfig.load();
-            RuleManager.load(server);
-        });
-
-        ServerLifecycleEvents.SERVER_STOPPED.register(server -> CommonClass.setServer(null));
-        ResourceManagerHelper.get(PackType.SERVER_DATA).registerReloadListener(new FabricReloadListener());
     }
 
-    private static class FabricReloadListener implements SimpleSynchronousResourceReloadListener {
-
-        @Override
-        public ResourceLocation getFabricId() {
-            return new ResourceLocation("mod_template", "reload_listener");
-        }
-
-        @Override
-        public void onResourceManagerReload(@NotNull ResourceManager manager) {
-            if (CommonClass.getServer() == null) {
-                return;
-            }
-
-            ModConfig.load();
-            RuleManager.load(CommonClass.getServer());
-        }
-
-        @Override
-        public Collection<ResourceLocation> getFabricDependencies() {
-            return Collections.singletonList(ResourceReloadListenerKeys.TAGS);
-        }
-    }
 }
--- forge/src/main/java/com/evandev/mod_template/ExampleMod.java
+++ forge/src/main/java/com/evandev/mod_template/ExampleMod.java
@@ -1,12 +1,6 @@
 package com.evandev.mod_template;
 
 import com.evandev.mod_template.client.ClientConfigSetup;
-import com.evandev.mod_template.config.ModConfig;
-import com.evandev.mod_template.logic.RuleManager;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TagsUpdatedEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -14,7 +8,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 
-@Mod(ExampleMod.MOD_ID)
+@Mod(Constants.MOD_ID)
 public class ExampleMod {
     public ExampleMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
